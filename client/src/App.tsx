77d5eb13ec3fe998363abbc4e4535681428e7501
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-27 12:45:53 -0500

    building out the forms that will eventually POST saved cities

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
+import {
+	Box,
+	Button,
+	Center,
+	FormControl,
+	FormHelperText,
+	FormLabel,
+	Input,
+	VStack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
 import './App.css';
 
 // const weatherURL = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API key}`
 
 function App() {
+	const [formInput, setFormInput] = useState<string | null>(null);
 	const weatherKey = process.env.REACT_APP_WEATHER_KEY;
+
 	type Coord = number;
 
 	const lat: Coord = 40.651282;
@@ -26,7 +39,22 @@ function App() {
 
 	return (
 		<div className="App">
-			<header className="App-header">Weather App</header>
+			<FormControl>
+				<VStack>
+					<Box>
+						<FormLabel htmlFor="city">City</FormLabel>
+						<Input
+							onChange={(e) => {
+								setFormInput(e.target.value);
+							}}
+							id="city"
+							type="city"
+						/>
+						<FormHelperText>Please enter a city</FormHelperText>
+						<Button onClick={() => {}}>Submit</Button>
+					</Box>
+				</VStack>
+			</FormControl>
 		</div>
 	);
 }
